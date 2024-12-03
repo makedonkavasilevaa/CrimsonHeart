ce3e0831c85a3f82f67ca36bfb95e0bececba8f7
Makedonka Vasileva <99400411+makedonkavasilevaa@users.noreply.github.com>
2024-12-03 10:50:03 +0100

    Initial commit

---
+++ src/main/java/mk/finki/ukim/mk/crimsonheart/CrimsonHeartApplication.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.mk.crimsonheart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimsonHeartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrimsonHeartApplication.class, args);
+    }
+
+}
