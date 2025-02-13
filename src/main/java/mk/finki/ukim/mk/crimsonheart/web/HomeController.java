70c34c2b638a189d5b83d56913adc42c1a711dba
Makedonka Vasileva <99400411+makedonkavasilevaa@users.noreply.github.com>
2025-02-13 15:30:02 +0100

    #28 - return 4 results

--- src/main/java/mk/finki/ukim/mk/crimsonheart/web/HomeController.java
+++ src/main/java/mk/finki/ukim/mk/crimsonheart/web/HomeController.java
@@ -2,6 +2,7 @@ package mk.finki.ukim.mk.crimsonheart.web;
 
 import mk.finki.ukim.mk.crimsonheart.model.DonationEvent;
 import mk.finki.ukim.mk.crimsonheart.service.DonationEventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
-    private final DonationEventService eventService;
-
-    public HomeController(DonationEventService eventService) {
-        this.eventService = eventService;
-    }
+    @Autowired
+    private DonationEventService eventService;
 
     @GetMapping("")
     public String getHomePage(@RequestParam(required = false) String error, Model model){
@@ -27,6 +26,12 @@ public class HomeController {
             model.addAttribute("error", error);
         }
         List<DonationEvent> events = this.eventService.listAll();
+
+        List<DonationEvent> latestEvents = events.stream()
+                .sorted((event1, event2) -> event2.getDateOfEvent().compareTo(event1.getDateOfEvent())) // assuming `getDate` gives you a comparable Date object
+                .limit(4)
+                .collect(Collectors.toList());
+
         model.addAttribute("bodyContent", "events");
         model.addAttribute("events", events);
         return "homepage";
